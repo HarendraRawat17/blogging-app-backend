f43bd25d5df3550b52b1d2b619f9461c9e6213e0
HarendraRawat17 <yt2022a3@gmail.com>
2026-05-17 11:33:51 +0530

    otp gamil related error

--- src/Services/emailService.js
+++ src/Services/emailService.js
@@ -3,13 +3,21 @@ import nodemailer from "nodemailer";
 
 // create a transporter using SMTP 
 const transporter = nodemailer.createTransport({
+  service: "gmail",
   host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true, 
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS,
   },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+  
+  family: 4, 
+
 });
 
 
